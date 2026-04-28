7902b81e5edd56263fca301c0570e3d9d8ef4b28
Areeb Ali <sayyedareeb90@gmail.com>
2026-04-28 22:45:40 +0530

    made changes in root directory

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = (process.env.REACT_APP_API_URL || "http://localhost:5000").replace(/\/$/, "");
+const isLocalhost = typeof window !== "undefined" && (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
+const defaultUrl = isLocalhost ? "http://localhost:5000" : "";
+const API_BASE_URL = (process.env.REACT_APP_API_URL || defaultUrl).replace(/\/$/, "");
 
 export default API_BASE_URL;
