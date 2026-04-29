2a0d1c8efacad814eb5ea04bf3ab918df41e1444
Areeb Ali <sayyedareeb90@gmail.com>
2026-04-29 11:59:06 +0530

    fixed port for render

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,3 @@
-const isLocalhost = typeof window !== "undefined" && (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
-const defaultUrl = isLocalhost ? "http://localhost:5000" : "";
-const API_BASE_URL = (process.env.REACT_APP_API_URL || defaultUrl).replace(/\/$/, "");
+const API_BASE_URL = (process.env.REACT_APP_API_URL || "http://localhost:5000").replace(/\/$/, "");
 
 export default API_BASE_URL;
